9a9c16330cf7f77226fdd71877f08486f43e2d14
NewNow974 <49619618+NewNow974@users.noreply.github.com>
2020-03-24 16:45:54 +0100

    Eloi2

--- formulaire.php
+++ formulaire.php
@@ -3,15 +3,41 @@
 
 class Formulaire
 {
-    // Méthodes
-    function __construct($nom1, $nbTitre) {
-        $this->nom = $nom1;
-        $this->nombreTitre = $nbTitre;
-        self::$nombreEquipe++;
+    private $html="";
+    private $interieur="";
+    function __construct($nom_Fichier, $methodeEnvoie) {
+
+        $this->html= "<form action=$nom_Fichier method=$methodeEnvoie>$this->interieur</form>";
     }
+
+    function ajouterzonetexte()
+    {
+        $this->interieur.="<input type='text'name='nom6'/>";
+    }
+
+    function ajouterbouton()
+    {
+        $this->interieur.="<input type='submit' value='Enregister'/>";
+    }
+
+    function getform()
+    {
+        echo $this->html;
+    }
+
     function __destruct(){
         echo "Fin\n";
     }
+
 }
+
+
+$p1= new Formulaire("fichier.php", "post");
+$p1->ajouterzonetexte();
+$p1->ajouterzonetexte();
+$p1->ajouterbouton();
+
+
+
 
 ?>
--- testformulaire.php
+++ testformulaire.php
@@ -1,4 +1,29 @@
+
 <?php
 
 
+class Formulaire
+{
+    private $html = "";
+    private $interieur = "";
+    function __construct($fichier, $method)
+    {
+        $this->html .= "<form method='$method' action='$fichier'>";
+    }
+    function ajouterzonetexte($text){
+        $this->html .= $text." <input type='text'name='nom6'/><br><br>";
+    }
+    function ajouterbouton(){
+        $this->html .= "<button>Cliquez ici</button>";
+    }
+    function getform(){
+        return $this->html . "</form>";
+    }
+}
+
+$p1 = new Formulaire("result.php","post");
+$p1->ajouterzonetexte("Votre nom :");
+$p1->ajouterzonetexte("Votre code :");
+$p1->ajouterbouton();
+echo $p1->getform();
 ?>
